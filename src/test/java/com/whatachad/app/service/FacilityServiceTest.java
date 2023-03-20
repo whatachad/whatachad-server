1afea5dfdccdc6f1a17469af3143448e5cbfc795
jinhyeon <vmflwu123@gmail.com>
2023-03-20 20:41:35 +0900

    test(FacilityServiceTest): update 리팩토링과 유저 필드 추가로 인한 테스트

--- src/test/java/com/whatachad/app/service/FacilityServiceTest.java
+++ src/test/java/com/whatachad/app/service/FacilityServiceTest.java
@@ -1,30 +1,59 @@
 package com.whatachad.app.service;
 
-import com.whatachad.app.model.domain.Address;
 import com.whatachad.app.model.domain.Facility;
-import com.whatachad.app.model.dto.CreateFacilityDto;
-import com.whatachad.app.model.dto.UpdateFacilityDto;
-import org.assertj.core.api.Assertions;
+import com.whatachad.app.model.request.CreateFacilityDto;
+import com.whatachad.app.model.request.UpdateFacilityDto;
+import com.whatachad.app.model.request.UserLoginRequestDto;
+import com.whatachad.app.model.response.UserTokenResponseDto;
+import com.whatachad.app.type.FacilityType;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FacilityServiceTest {
 
     @Autowired
     private FacilityService facilityService;
+    @Autowired
+    private TokenService tokenService;
 
     @BeforeEach
-    void initFacility() {
-        CreateFacilityDto dto = CreateFacilityDto.builder()
-                .address(new Address("도로주소", "지번주소", "지역코드", "위도", "경도"))
+    void initFacility() throws Exception {
+        UserLoginRequestDto loginDto = UserLoginRequestDto.builder()
+                .id("admin")
+                .password("admin")
                 .build();
-        facilityService.createFacility(dto);
+        UserTokenResponseDto token = UserTokenResponseDto.builder()
+                .accessToken(tokenService.genAccessToken(loginDto.getId()))
+                .refreshToken(tokenService.genRefreshToken(loginDto.getId()))
+                .build();
+        Claims claims = tokenService.validateToken(token.getAccessToken());
+        List<String> authorities = (List) claims.get("authorities");
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        CreateFacilityDto createFacilityDto = CreateFacilityDto.builder()
+                .jibunAddress("지번주소")
+                .lat("0.0")
+                .lng("0.0")
+                .category(FacilityType.HEALTH)
+                .build();
+        facilityService.createFacility(createFacilityDto);
     }
 
     @Test
@@ -32,12 +61,22 @@ class FacilityServiceTest {
     void updateFacility() {
         Facility facility = facilityService.findAllFacilities().get(0);
         UpdateFacilityDto dto = UpdateFacilityDto.builder()
-                .address(new Address("변경된 도로주소", "지번주소", "지역코드", "위도", "경도"))
+                .jibunAddress("변경된 주소")
+                .lat("0.0")
+                .lng("0.0")
+                .category(FacilityType.HEALTH)
                 .build();
         facilityService.updateFacility(facility.getId(), dto);
 
         Facility updateFacility = facilityService.findFacility(facility.getId());
-        assertThat(updateFacility.getAddress().getRoadAddress()).isEqualTo("변경된 도로주소");
+        assertThat(updateFacility.getAddress().getJibunAddress()).isEqualTo("변경된 주소");
+    }
+
+    @Test
+    @DisplayName("facility를 등록할 때 facility 엔티티와 유저 엔티티의 연관 관계가 맺어진다.")
+    void createFacilityWithLoginUser() {
+        Facility facility = facilityService.findAllFacilities().get(0);
+        assertThat(facility.getUser().getId()).isEqualTo("admin");
     }
 
 }
