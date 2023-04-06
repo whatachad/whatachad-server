acb1a7308d1493ffb68fc1fa72edd75789920ad4
jinhyeon <vmflwu123@gmail.com>
2023-04-06 15:43:25 +0900

    feat($FacilityType): 스포츠 시설 종류 추가

--- src/main/java/com/whatachad/app/type/FacilityType.java
+++ src/main/java/com/whatachad/app/type/FacilityType.java
@@ -11,7 +11,23 @@ public enum FacilityType {
     YOGA,
     CLIMBING,
     PULL_UP_BAR,
-    PARALLEL_BAR;
+    PARALLEL_BAR,
+    TENNIS,
+    TABLE_TENNIS,
+    BADMINTON,
+    FOOTBALL,
+    SOCCER,
+    BASEBALL,
+    BOWLING,
+    BILLIARD,
+    GOLF,
+    BOXING,
+    KICK_BOXING,
+    JUJITSU,
+    MUAY_THAI,
+    MMA,
+    ETC;
+
 
     public static boolean contains(String value) {
         List<String> allTypeNames = List.of(
@@ -21,7 +37,22 @@ public enum FacilityType {
                 YOGA.name(),
                 CLIMBING.name(),
                 PULL_UP_BAR.name(),
-                PARALLEL_BAR.name());
+                PARALLEL_BAR.name(),
+                TENNIS.name(),
+                TABLE_TENNIS.name(),
+                BADMINTON.name(),
+                FOOTBALL.name(),
+                SOCCER.name(),
+                BASEBALL.name(),
+                BOWLING.name(),
+                BILLIARD.name(),
+                GOLF.name(),
+                BOXING.name(),
+                KICK_BOXING.name(),
+                JUJITSU.name(),
+                MUAY_THAI.name(),
+                MMA.name(),
+                ETC.name());
         return allTypeNames.contains(value);
     }
 
