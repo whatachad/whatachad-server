bde92f69dc9b61e4534b6b9b3c14a5153f080e60
jinhyeon <vmflwu123@gmail.com>
2023-05-02 18:47:02 +0900

    feat($AccountType, $DayworkPriority): enum value 추가

--- src/main/java/com/whatachad/app/type/AccountType.java
+++ src/main/java/com/whatachad/app/type/AccountType.java
@@ -1,5 +1,16 @@
 package com.whatachad.app.type;
 
+import lombok.Getter;
+
+@Getter
 public enum AccountType {
-    SPEND, INCOME
+
+    SPEND(0),
+    INCOME(1);
+
+    private final int value;
+
+    AccountType(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/whatachad/app/type/DayworkPriority.java
+++ src/main/java/com/whatachad/app/type/DayworkPriority.java
@@ -1,5 +1,18 @@
 package com.whatachad.app.type;
 
+import lombok.Getter;
+
+@Getter
 public enum DayworkPriority {
-    FIRST, SECOND, THIRD
+
+    FIRST(1),
+    SECOND(2),
+    THIRD(3);
+
+    private final int value;
+
+    DayworkPriority(int value) {
+        this.value = value;
+    }
+
 }
