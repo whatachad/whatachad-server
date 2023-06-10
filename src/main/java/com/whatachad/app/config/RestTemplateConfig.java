c548f9413af713ba8296958daa1ff0311fe95135
jinhyeon <vmflwu123@gmail.com>
2023-06-11 01:20:16 +0900

    refactor($LocalMapService): RestTemplate을 스프링 빈으로 등록
    LocalMapService에 사용되는 RestTemplate 설정을
    다른 클래스로 분리해서 사용

---
+++ src/main/java/com/whatachad/app/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package com.whatachad.app.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    private static final String REST_API_KEY = "58d92c624b9a0af0d99c01bece4f2b3b";
+
+    @Bean
+    public RestTemplate localMapRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .defaultHeader("Authorization", "KakaoAK " + REST_API_KEY)
+                .additionalMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .build();
+    }
+}
--- src/main/java/com/whatachad/app/service/LocalMapService.java
+++ src/main/java/com/whatachad/app/service/LocalMapService.java
@@ -1,56 +1,50 @@
 package com.whatachad.app.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whatachad.app.common.BError;
 import com.whatachad.app.common.CommonException;
 import com.whatachad.app.model.response.ReverseGeocodeDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class LocalMapService {
 
     private static final double EARTH_RADIUS = 6371;
-    private static final String REST_API_KEY = "58d92c624b9a0af0d99c01bece4f2b3b";
-    private final ObjectMapper objectMapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private static final String REVERSE_GEOCODE_API_URL = "https://dapi.kakao.com/v2/local/geo/coord2address.json";
+    @Qualifier("localMapRestTemplate")
+    private final RestTemplate restTemplate;
+
 
     public String reverseGeocode(double lat, double lng) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            String requestUrl = "https://dapi.kakao.com/v2/local/geo/coord2address.json";
-            requestUrl += "?y=" + lat;
-            requestUrl += "&x=" + lng;
+        String requestUrl = UriComponentsBuilder.fromHttpUrl(REVERSE_GEOCODE_API_URL)
+                .queryParam("x", lng)
+                .queryParam("y", lat)
+                .encode(StandardCharsets.UTF_8)
+                .toUriString();
 
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.set("Authorization", "KakaoAK " + REST_API_KEY);
-            HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
-            ResponseEntity<String> response = restTemplate
-                    .exchange(requestUrl, HttpMethod.GET, httpEntity, String.class);
-            ReverseGeocodeDto reverseGeocodeDto = objectMapper.readValue(response.getBody(), ReverseGeocodeDto.class);
-            return reverseGeocodeDto
-                    .getDocuments()
-                    .get(0)
-                    .getAddress()
-                    .getAddress_name();
-        } catch (JsonProcessingException e) {
-            throw new CommonException(BError.FAIL_REASON, "reverse-geocode",
-                    "cannot parse reverse-geocode response");
-        }
+        ReverseGeocodeDto response = restTemplate.getForObject(requestUrl, ReverseGeocodeDto.class);
+        return Objects.requireNonNull(response)
+                .getDocuments()
+                .get(0)
+                .getAddress()
+                .getAddress_name();
     }
 
     /**
